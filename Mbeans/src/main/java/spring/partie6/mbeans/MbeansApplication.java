6c21a89c8ba612ff36f4c3dae8395c6a75d23ad4
tarik mamou <tarik339@gmail.com>
2021-09-10 09:39:26 +0200

    * suite au commit precedent

--- Mbeans/src/main/java/spring/partie6/mbeans/MbeansApplication.java
+++ Mbeans/src/main/java/spring/partie6/mbeans/MbeansApplication.java
@@ -3,7 +3,7 @@ package spring.partie6.mbeans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+// example simple a visualiser avec jconsole
 @SpringBootApplication
 public class MbeansApplication {
 
